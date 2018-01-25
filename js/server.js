a62251955bab4a387e623d46aaead82a7b6691d7
jblau07 <jlau7248@gmail.com>
2018-01-25 11:51:27 -1000

    client side functionality working

--- js/server.js
+++ js/server.js
@@ -1,44 +1,93 @@
 const net = require('net');
 
+const {
+  index,
+  _404,
+  helium,
+  hydrogen,
+  styles
+} = require('./pages');
+
 const server = net.createServer((socket) => {
   console.log('Client connected');
-  socket.on('end', () => {
-    console.log('Client disconnected');
-  })
 
   socket.on('data', (chunk) => {
-    let dataArr = parseData(chunk)
-    let firstLine = dataArr[0].split(" ");
-    //console.log(firstLine);
-    let reqMethod = firstLine[0];
-    console.log(reqMethod);
-    let reqAddress = firstLine[1];
-    console.log(reqAddress);
-    let reqHttp = firstLine[2];
-    console.log(reqHttp);
-
-    if (reqMethod === 'GET'){
-      switch (reqAddress) {
-        
-      }
-    }
-    //let header = 
-
-
-    // let reqMethod = dataArr[0];
-    // let reqAddress = dataArr[1];
-   // let reqHTTP = dataArr[2].split('\');
-    //console.log(dataArr);
-   
+    socket.write(responseHandler(chunk));
+    socket.end();
+    socket.on('end', () => {
+      console.log('Client disconnected');
+    })
   })
 });
 
-//process.stdin.pipe(server);
-
-server.listen(8080, 'localhost',() => {
+server.listen(8080, 'localhost', () => {
   console.log('server bound');
 });
 
-let parseData = (data) => {
-  return data.toString().split('\r\n');
- };
+let responseHandler = (info) => {
+  let dataArr = info.toString().split('\r\n');
+
+  let firstLine = dataArr[0].split(" ");
+  let reqMethod = firstLine[0];
+  let reqURI = firstLine[1];
+
+  let insertDate = new Date().toUTCString();
+  let reqHttp = 'HTTP/1.1';
+  let okStatus = '200 OK';
+  let badStatus = '404 Not Found';
+  let serverName = 'Justin Server';
+
+  let headerLine1Good = reqHttp + ' ' + okStatus;
+  let headerLine1Bad = reqHttp + ' ' + badStatus;
+  let headerLine2 = 'Date: ' + insertDate;
+  let headerLine3 = 'Server: ' + serverName;
+
+  let goodResponseHeader = `${headerLine1Good}\n${headerLine2}\n${headerLine3}\n\n`;
+  let badResponseHeader = `${headerLine1Bad}\n${headerLine2}\n${headerLine3}\n\n`;
+
+  switch (reqURI) {
+    case '/':
+      return goodResponseHeader + index;
+      break;
+    case '/index.html':
+      return goodResponseHeader + index
+      break;
+    case '/hydrogen.html':
+      return goodResponseHeader + hydrogen
+      break;
+    case '/helium.html':
+      return goodResponseHeader + helium;
+      break;
+    case '/css/styles.css':
+      return goodResponseHeader + styles;
+      break;
+    default:
+      return badResponseHeader + _404;
+  }
+}
+// switch (reqURI) {
+//   case '/':
+//     socket.write(goodResponseHeader + index);
+//     socket.end();
+//     break;
+//   case 'index':
+//     socket.write(goodResponseHeader + index);
+//     socket.end();
+//     break;
+//   case 'hydrogen':
+//     socket.write(goodResponseHeader + hydrogen)
+//     socket.end();
+//     break;
+//   case 'helium':
+//     socket.write(goodResponseHeader + helium);
+//     socket.end();
+//     break;
+//   case 'styles':
+//     socket.write(goodResponseHeader + styles);
+//     socket.end();
+//     break;
+//   default:
+//     socket.write(badResponseHeader + _404);
+//     socket.end();
+//     break;
+// }
